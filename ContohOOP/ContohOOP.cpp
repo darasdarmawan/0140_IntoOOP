4bc873de8b2a17d94960a9e20ec2a4f070125e3b
darasdarmawan <darasyauqidarmawan@gmail.com>
2024-04-22 15:08:26 +0700

    membuat void input

--- ContohOOP/ContohOOP.cpp
+++ ContohOOP/ContohOOP.cpp
@@ -6,3 +6,10 @@ class bangunDatar {
 		float panjang, lebar;
 public:
 	float luas;
+
+void input() {
+	cout << "Masukan Panjangnya = ";
+	cin >> panjang;
+	cout << "Masukan Lebarnya = ";
+	cin >> lebar;
+}
